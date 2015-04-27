3a466ed04b40d7a1bf8397a3ae42c1586de44fe9
Christy Perez <christy@linux.vnet.ibm.com>
2015-04-27 11:10:36 -0500

    Create VMs Asynchronously: Tests
    Signed-off-by: Christy Perez <christy@linux.vnet.ibm.com>

--- tests/test_mockmodel.py
+++ tests/test_mockmodel.py
@@ -66,7 +66,9 @@ class MockModelTests(unittest.TestCase):
         req = json.dumps({'name': 'test', 'cdrom': fake_iso})
         request(host, ssl_port, '/templates', req, 'POST')
         req = json.dumps({'name': 'test-vm', 'template': '/templates/test'})
-        request(host, ssl_port, '/vms', req, 'POST')
+        resp = request(host, ssl_port, '/vms', req, 'POST')
+        task = json.loads(resp.read())
+        wait_task(model.task_lookup, task['id'])
 
         # Test screenshot refresh for running vm
         request(host, ssl_port, '/vms/test-vm/start', '{}', 'POST')
@@ -94,7 +96,9 @@ class MockModelTests(unittest.TestCase):
         def add_vm(name):
             # Create a VM
             req = json.dumps({'name': name, 'template': '/templates/test'})
-            request(host, ssl_port, '/vms', req, 'POST')
+            task = json.loads(request(host, ssl_port, '/vms', req,
+                              'POST').read())
+            wait_task(model.task_lookup, task['id'])
 
         vms = [u'abc', u'bca', u'cab', u'xba']
         for vm in vms:
@@ -106,7 +110,9 @@ class MockModelTests(unittest.TestCase):
     def test_vm_info(self):
         model.templates_create({'name': u'test',
                                 'cdrom': fake_iso})
-        model.vms_create({'name': u'test-vm', 'template': '/templates/test'})
+        task = model.vms_create({'name': u'test-vm',
+                                 'template': '/templates/test'})
+        wait_task(model.task_lookup, task['id'])
         vms = model.vms_get_list()
         self.assertEquals(2, len(vms))
         self.assertIn(u'test-vm', vms)
